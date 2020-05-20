8d0a901cb46e0cd7dee353a474673abf547d6d3f
rye gaspar <gaspar.ryan@outlook.com>
2020-05-20 17:18:03 -0500

    09 - rendering tweets on the timeline

--- app/User.php
+++ app/User.php
@@ -41,6 +41,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function avatar()
+    {
+        return 'https://www.gravatar.com/avatar/' . md5($this->email) . '?d=mp';
+    }
+
     public function following()
     {
         return $this->belongsToMany(
