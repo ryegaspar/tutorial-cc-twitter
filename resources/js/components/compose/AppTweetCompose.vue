bc08730b06eb6f4d87846def9391ecd2b446be7d
rye gaspar <gaspar.ryan@outlook.com>
2020-05-27 10:04:30 -0500

    47 - preparing media to be uploaded

--- resources/js/components/compose/AppTweetCompose.vue
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -65,9 +65,36 @@
 
         methods: {
             async submit() {
+                let media = await this.uploadMedia()
+                this.form.media = media.data.data.map(r => r.id);
+
                 await axios.post('/api/tweets', this.form)
 
                 this.form.body = ''
+            },
+
+            async uploadMedia() {
+                return await axios.post('/api/media', this.buildMediaForm(), {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                });
+            },
+
+            buildMediaForm() {
+                let form = new FormData();
+
+                if (this.media.images.length) {
+                    this.media.images.forEach((image, index) => {
+                        form.append(`media[${index}]`, image)
+                    });
+                }
+
+                if (this.media.video) {
+                    form.append('media[0]', this.media.video);
+                }
+
+                return form;
             },
 
             async getMediaTypes() {
